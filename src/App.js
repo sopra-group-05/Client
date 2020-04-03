bae2fb4edc45653da43d70851750e4fbeb0f272d
Florian <38964049+Floribur@users.noreply.github.com>
2020-04-03 12:18:47 +0200

    Remove Header from previous Design

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header height={"200"} />
+        /*<Header height={"200"} />*/
         <AppRouter />
       </div>
     );
