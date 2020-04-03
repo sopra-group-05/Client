64407d4724c2485b7ca11302fa3edc06b229e5cf
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-04-03 18:19:15 +0200

    Add Username to localstorage

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -126,6 +126,7 @@ class Login extends React.Component {
       // Store the token into the local storage.
       localStorage.setItem("token", user.token);
       localStorage.setItem("userId", user.id);
+      localStorage.setItem("username", user.username);
 
       // Login successfully worked --> navigate to the route /game in the GameRouter
       this.props.history.push(`/game`);
