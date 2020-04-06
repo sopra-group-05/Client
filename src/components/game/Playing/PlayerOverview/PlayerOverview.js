4d2a9e531fe43afdf0fc662cb466082f1ef7911b
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-04-06 22:09:12 +0200

    Create Basic PlayerOverview

---
+++ src/components/game/Playing/PlayerOverview/PlayerOverview.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import { withRouter } from "react-router-dom";
+import Lobby from "../../../shared/models/Lobby";
+import Box from "../../../../views/Box";
+
+const PlayerOverview = ({ l }) => {
+  const lobby = new Lobby(l); //transform input into Lobby Model
+  // todo: implement PlayerOverview.
+  return (
+    <Box maxWidth="350px" title="PlayerOverview">
+      {JSON.stringify(lobby, null, 2)}
+    </Box>
+  );
+};
+
+export default withRouter(PlayerOverview);
