c97a18614c6f1e5c98a24d067e1d928ac8a8b008
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-04-06 11:15:02 +0200

    Prettier Formatting

--- src/views/Box.js
+++ src/views/Box.js
@@ -36,12 +36,18 @@ const BoxContainer = styled.div`
  */
 const Box = ({ children, title, titleWidth, titleAlign, texttrans }) => {
   const boxTitle = title ? title : "No Title";
-  const headerTitleWidth = titleWidth ? titleWidth : 'auto';
-  const align = titleAlign ? titleAlign : 'center';
-  const textTransformation = texttrans ? '': 'uppercase';
+  const headerTitleWidth = titleWidth ? titleWidth : "auto";
+  const align = titleAlign ? titleAlign : "center";
+  const textTransformation = texttrans ? "" : "uppercase";
   return (
     <BoxOuter>
-      <BoxHeader style={{width:headerTitleWidth, textAlign:align, textTransform: textTransformation}}>
+      <BoxHeader
+        style={{
+          width: headerTitleWidth,
+          textAlign: align,
+          textTransform: textTransformation
+        }}
+      >
         <BoxTitle>{boxTitle}</BoxTitle>
       </BoxHeader>
       <BoxContainer>{children}</BoxContainer>
