74488fa9bf16c63fbab2240844530234da297eda
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-04-06 22:09:00 +0200

    Add Style for PlayerContainer and Children

---
+++ src/components/game/Playing/PlayingStyle.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin-right: 2rem;
+  width: 100%;
+  flex-grow: 2;
+`;
+
+export const PlayingContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  width: 100%;
+`;
+
+export const PlayingTitle = styled.h3`
+  font-size: 3rem;
+  font-weight: bold;
+  padding: 0;
+  margin: 0;
+`;
+
+export const PlayingDescription = styled.p`
+  font-size: 1.3rem;
+  font-weight: normal;
+  padding: 0;
+  margin: 0;
+`;
