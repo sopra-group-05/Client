39be406918b2d571c1d32f0e85a1003947402362
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-04-09 14:35:43 +0200

    FS20G05-10 Replace old Styling with PlayingWrapper

--- src/components/game/Playing/ChoosingMysteryWord/WaitForNumber.js
+++ src/components/game/Playing/ChoosingMysteryWord/WaitForNumber.js
@@ -1,30 +1,27 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import Lobby from "../../../shared/models/Lobby";
-import Box from "../../../../views/Box";
 import {
-  PlayingContent,
   PlayingDescription,
   PlayingTitle,
-  Wrapper
+  PlayingWrapper
 } from "../PlayingStyle";
 import MysteryCard from "./MysteryCard";
+import styled from "styled-components";
 
 const WaitForNumber = ({ l }) => {
   const lobby = new Lobby(l); //transform input into Lobby Model
   return (
-    <Wrapper>
-      <Box hideHeader={true}>
-        <PlayingContent>
-          <PlayingTitle>Teammate</PlayingTitle>
-          <PlayingDescription>
-            You will try to come up with a hint for the mystery word so that the
-            active Player can guess it.
-          </PlayingDescription>
-          <MysteryCard lobbyId={lobby.id} />
-        </PlayingContent>
-      </Box>
-    </Wrapper>
+    <PlayingWrapper headerText="Active Player is choosing a number!">
+      <PlayingTitle>Teammate</PlayingTitle>
+      <PlayingDescription>
+        You will try to come up with a hint for the mystery word
+      </PlayingDescription>
+      <PlayingDescription>
+        so that the active Player can guess it.
+      </PlayingDescription>
+      <MysteryCard lobbyId={lobby.id} />
+    </PlayingWrapper>
   );
 };
 
