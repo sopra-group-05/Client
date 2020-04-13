864f9541f3add61a8f8937524087f1de92537909
Adiboeh <adillamzee@hotmail.com>
2020-04-13 03:29:19 +0200

    FS20G05-7 PlayerOverview: Updated Playeroverview with Players and Buttons, added PlayerInOverviews

--- src/components/game/Playing/PlayerOverview/PlayerOverview.js
+++ src/components/game/Playing/PlayerOverview/PlayerOverview.js
@@ -3,13 +3,49 @@ import styled from "styled-components";
 import { withRouter } from "react-router-dom";
 import Lobby from "../../../shared/models/Lobby";
 import Box from "../../../../views/Box";
+import PlayerInOverview from "../../../../views/PlayerInOverview";
+import {BaseContainer} from "../../../../helpers/layout";
+import {Button} from "../../../../views/design/Button";
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+`;
+const ButtonSpacer = styled.div`
+  width: 14em
+`;
 
 const PlayerOverview = ({ l }) => {
   const lobby = new Lobby(l); //transform input into Lobby Model
   // todo: implement PlayerOverview.
   return (
     <Box maxWidth="350px" title={lobby.lobbyName}>
-      {JSON.stringify(lobby, null, 2)}
+
+        <React.Fragment>
+            {l.players.map(player => {
+                return (
+                        <PlayerInOverview player={player} lobby={lobby} />
+                );
+            })}
+            <ButtonContainer>
+                <Button
+                    colorDef={"red"}
+                    onClick={() => {;
+                    }}
+                >
+                    Leave
+                </Button>
+                <ButtonSpacer/>
+                <Button
+                    colorDef={"#3b85ff"}
+                    onClick={() => {;
+                    }}
+                >
+                    Rules
+                </Button>
+            </ButtonContainer>
+        </React.Fragment>
     </Box>
   );
 };
---
+++ src/views/PlayerInOverview.js
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components";
+import Avatar from "./Avatar";
+
+const Container = styled.div`
+  margin-top: 1rem;
+  padding: 1rem;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: row;
+  background-color: #454c62;
+  a {
+    color: #ce552e;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    min-width: 300px;
+    text-decoration: none;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const PlayerName = styled.p`
+  font-weight: bold;
+  margin: 0 10px;
+  color: #fff;
+  min-width: 50px;
+`;
+
+const Icon = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-top: 1rem;
+  align-self: flex-end;
+`;
+
+const IconPlaceholder = styled.div`
+  width: 25px;
+  height: 25px;
+  margin-top: 1rem;
+  align-self: flex-end;
+`;
+
+const PlayerInfo = styled.p`
+  font-weight: lighter;
+  font-size: 0.8rem;
+  margin: 0 10px;
+  color: #8f8f8f;
+`;
+
+const PlayerMeta = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 1rem;
+  margin-right: 0.5rem;
+`;
+
+class PlayerInOverview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: this.props.player,
+      lobby: this.props.lobby,
+      creator: this.props.lobby.creator,
+      error: null
+    };
+  }
+
+
+  render() {
+    return (
+      <Container>
+        <Avatar size={40} user={this.state.player} />
+        <PlayerMeta>
+          <PlayerName>{this.state.player.username}</PlayerName>
+          <PlayerInfo>
+            {this.state.player.role} | {this.state.player.status}
+          </PlayerInfo>
+        </PlayerMeta>
+      </Container>
+    );
+  }
+}
+
+export default PlayerInOverview;
