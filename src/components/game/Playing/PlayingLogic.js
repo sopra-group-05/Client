f6087bd60b19b22c285a6bf747b027678b536dad
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-04-29 18:14:48 +0200

    First Try to get PlayingLogic to work with PlayerStatus

--- src/components/game/Playing/PlayingLogic.js
+++ src/components/game/Playing/PlayingLogic.js
@@ -5,6 +5,7 @@ import ClueComparison from "./ClueComparison/ClueComparison";
 import Guess from "./Guess/Guess";
 import React from "react";
 import EndRound from "./EndRound/EndRound";
+import Lobby from "../../shared/models/Lobby";
 
 const PlayingLogic = ({ state, nextState, lobby, isGuesser }) => {
   // this component renders the correct state of the game
@@ -15,10 +16,21 @@ const PlayingLogic = ({ state, nextState, lobby, isGuesser }) => {
     Guess,
     EndRound
   ]; // add your views at correct position
-  state = state % states.length;
+
+  lobby = new Lobby(lobby);
+  if (lobby.players.some(player => player.status === "PICKING_NUMBER")) {
+    state = 0;
+  } else if (lobby.players.some(player => player.status === "WRITING_CLUES")) {
+    state = 1;
+  } else {
+    console.log("fallback-state!");
+    state = state % states.length;
+  }
+
   const Component = states[state];
   // todo: make some kind of if statement to switch between number, clues etc by the status of the LOBBY!
   // todo: this is just to show how it would look like when the API works.
+
   return (
     <Component lobby={lobby} isGuesser={isGuesser} nextState={nextState} />
   );
