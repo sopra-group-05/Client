874258833fb0a4cec11a6dadcf8c1e875a6ecf5c
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-04-30 12:50:11 +0200

    Reactivate Preview for next state

--- src/components/game/Playing/PlayingLogic.js
+++ src/components/game/Playing/PlayingLogic.js
@@ -9,40 +9,38 @@ import Lobby from "../../shared/models/Lobby";
 
 const PlayingLogic = ({ state, nextState, lobby, isGuesser }) => {
   // this component renders the correct state of the game
-  const old_states = [
-    ChoosingMysteryWord,
-    WritingClues,
-    ClueComparison,
-    Guess,
-    EndRound
-  ]; // add your views at correct position
   let Component = ChoosingMysteryWord;
-  const states = new Map();
-  // set States and which Component should be rendered given the Status of a player.
-  states.set("PICKING_NUMBER", ChoosingMysteryWord);
-  states.set("WAITING_FOR_NUMBER", ChoosingMysteryWord);
+  if (state) {
+    // todo remove when backend works
+    // this part is for the previewing phase. it will be removed once all part of the backend work
+    const old_states = [
+      ChoosingMysteryWord,
+      WritingClues,
+      ClueComparison,
+      Guess,
+      EndRound
+    ]; // add your views at correct position
+    state = state % old_states.length;
+    Component = old_states[state];
+  } else {
+    const states = new Map();
+    // set States and which Component should be rendered given the Status of a player.
+    states.set("PICKING_NUMBER", ChoosingMysteryWord);
+    states.set("WAITING_FOR_NUMBER", ChoosingMysteryWord);
 
-  states.set("WAITING_FOR_CLUES", WritingClues);
-  states.set("WRITING_CLUES", WritingClues);
+    states.set("WAITING_FOR_CLUES", WritingClues);
+    states.set("WRITING_CLUES", WritingClues);
 
-  states.set("WAITING_FOR_REVIEW", ClueComparison);
-  states.set("REVIEWING_CLUES", ClueComparison);
+    states.set("WAITING_FOR_REVIEW", ClueComparison);
+    states.set("REVIEWING_CLUES", ClueComparison);
 
-  // Select Component to render depending on the players OWN Status!
-  lobby = new Lobby(lobby);
-  let player = lobby.players.find(
-    player => player.id == localStorage.getItem("userId")
-  );
-  Component = states.get(player.status);
-
-  /*lobby.players.forEach(player => {
-    if (states.get(player.status)) {
-      Component = states.get(player.status);
-    }
-  });*/
-
-  // todo: make some kind of if statement to switch between number, clues etc by the status of the LOBBY!
-  // todo: this is just to show how it would look like when the API works.
+    // Select Component to render depending on the players OWN Status!
+    lobby = new Lobby(lobby);
+    let player = lobby.players.find(
+      player => player.id == localStorage.getItem("userId")
+    );
+    Component = states.get(player.status);
+  }
 
   return (
     <Component lobby={lobby} isGuesser={isGuesser} nextState={nextState} />
