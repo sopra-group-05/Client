1f85983f59189bef99ee30df3498bb908cb47d5a
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-04-30 19:22:34 +0200

    Add Compare Vorschlag to File

--- src/components/game/Playing/ClueComparison/ClueComparison.js
+++ src/components/game/Playing/ClueComparison/ClueComparison.js
@@ -2,12 +2,13 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import WaitForClues from "./WaitForClues";
 import CompareClues from "./CompareClues";
+import CompareCluesVorschlag from "./CompareClueVorschlag";
 
 const ClueComparison = ({ lobby, isGuesser, nextState }) => {
   if (isGuesser) {
     return <WaitForClues l={lobby} nextState={nextState} />;
   } else {
-    return <CompareClues l={lobby} nextState={nextState} />;
+    return <CompareCluesVorschlag l={lobby} nextState={nextState} />;
   }
 };
 
