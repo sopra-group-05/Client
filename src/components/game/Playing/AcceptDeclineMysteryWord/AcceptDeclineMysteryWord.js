d23ba2e8743cc247cd8cd288dd979ec172faf112
Florian Burkhardt <38964049+Floribur@users.noreply.github.com>
2020-05-18 14:55:06 +0200

    Fix Bug to avoid multiple POST requests

--- src/components/game/Playing/AcceptDeclineMysteryWord/AcceptDeclineMysteryWord.js
+++ src/components/game/Playing/AcceptDeclineMysteryWord/AcceptDeclineMysteryWord.js
@@ -27,20 +27,23 @@ const AcceptDeclineMysteryWord = ({ lobby, isGuesser }) => {
   const [submitted, setSubmitted] = React.useState(false);
 
   const makeRequest = async acceptWord => {
-    setSubmitted(true);
-    try {
-      // make POST request to Server to accept or decline mystery word
-      api.defaults.headers.common["Token"] = localStorage.getItem("token"); // set token to be allowed to request
-      if (acceptWord) {
-        console.log(acceptWord + " accepting word");
-        await api.post("/lobbies/" + lobby.id + "/acceptword");
-      } else {
-        console.log(acceptWord + " declining word");
-        await api.post("/lobbies/" + lobby.id + "/declineword");
+    if (!submitted) {
+      // only allow request when not yet submitted or there was an error.
+      setSubmitted(true);
+      try {
+        // make POST request to Server to accept or decline mystery word
+        api.defaults.headers.common["Token"] = localStorage.getItem("token"); // set token to be allowed to request
+        if (acceptWord) {
+          console.log(acceptWord + " accepting word");
+          await api.post("/lobbies/" + lobby.id + "/acceptword");
+        } else {
+          console.log(acceptWord + " declining word");
+          await api.post("/lobbies/" + lobby.id + "/declineword");
+        }
+      } catch (error) {
+        console.log(error.response ? error.response : "Unknown error");
+        setSubmitted(false);
       }
-    } catch (error) {
-      console.log(error.response ? error.response : "Unknown error");
-      setSubmitted(false);
     }
   };
 
